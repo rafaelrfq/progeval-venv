6cf8062589253b27b39ed8704e885bfeeecbad22
rafaelrfq <rafaelrfq@gmail.com>
2020-04-05 16:40:54 -0500

    heroku build 2

--- progeval_ing/settings.py
+++ progeval_ing/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1ap_7&s)0b^)+ymj!&-fc5ol@(%2mey4=%xq6+na*(e8ia7e^i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['progeval.herokuapp.com', '127.0.0.1']
 
